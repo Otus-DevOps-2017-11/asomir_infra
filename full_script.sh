d5a7f40f166054a61fccd77412364f66d74f157b
Alexander Akilin <asomirl@gmail.com>
2017-12-22 00:35:37 +0300

    Edit scripts

--- full_script.sh
+++ full_script.sh
@@ -8,7 +8,7 @@ gcloud compute instances create reddit-app-2\
   --zone=europe-west1-d \
   --metadata startup-script='#!/bin/bash
 cd ~
-wget https://raw.githubusercontent.com/Otus-DevOps-2017-11/iPrior_infra/Infra-2/startup_script.sh
+wget https://github.com/Otus-DevOps-2017-11/asomir_infra/blob/Infra-2/startup_script.sh
 sudo chmod +x ~/startup_script.sh
 sudo sh startup_script.sh
 '
--- startup_script.sh
+++ startup_script.sh
@@ -6,9 +6,9 @@ cd ~
 git clone https://github.com/Otus-DevOps-2017-11/asomir_infra.git
 cd ~/asomir_infra 
 git checkout Infra-2
-sudo chmod install_ruby.sh
-sudo chmod install_mongo.sh
-sudo chmod deploy.sh
+sudo chmod +x install_ruby.sh
+sudo chmod +x install_mongo.sh
+sudo chmod +x deploy.sh
 install_ruby.sh
 install_mongo.sh
 deploy.sh
